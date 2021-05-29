77c34e9230bc3342a98753f3c372a9376b153df9
reno <renan.modenese@gmail.com>
2021-05-28 23:21:12 -0300

    Incluindo CustomUser e CustomUserManager

--- users/models.py
+++ users/models.py
@@ -1,12 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.utils.translation import ugettext_lazy as _
+
+
+class CustomUserManager(UserManager):
+    '''Custom user manager. Extends de default UserManager.'''
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return super()._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        return super()._create_user(username, email, password, **extra_fields)
+
 
 class CustomUser(AbstractUser):
     '''Custom user model.
 
     CustomUser inherits the following fields from AbstractUser:
-    username, first_name, last_name, is_staff, date_joined
+    username, first_name, last_name, is_staff, is_superuser, date_joined
 
     The fields bellow are overwritten to allow login with email
     and email confirmation.
@@ -18,6 +34,8 @@ class CustomUser(AbstractUser):
     )
     is_active = models.BooleanField(_('is active'), default=False)
     # Add other user fields here
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.username
