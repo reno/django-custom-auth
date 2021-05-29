58c6edc29594ed2a9d278512e484f9ec500f7d87
reno <renan.modenese@gmail.com>
2021-05-29 15:56:29 -0300

    Incluindo backend de autenticação customizado

--- project/settings.py
+++ project/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = 'users.CustomUser'
 
 AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
+    'users.backends.CustomBackend'
 ]
 
 LOGIN_REDIRECT_URL = '/'
